515c2a6e1733ea2b42e7d882788f70f2008d45c5
Irvin <vinzu24@gmail.com>
2022-01-11 15:39:51 -0500

    added screens

--- App.js
+++ App.js
@@ -1,76 +1,9 @@
-import { StatusBar } from "expo-status-bar";
-import {
-  Alert,
-  Button,
-  Dimensions,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-  Image,
-  SafeAreaView,
-} from "react-native";
-import {
-  useDeviceOrientation,
-  useDimensions,
-} from "@react-native-community/hooks";
+// import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import WelcomeSreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  let x = 1;
   console.log("App executed");
-  console.log(require("./assets/icon.png"));
-  console.log(Dimensions.get("screen"));
-  console.log(useDimensions);
-  const handlePress = () => console.log("Text pressed");
-  const { landscape } = useDeviceOrientation();
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar style="auto" />
-      <Text numberOfLines={1} onPress={handlePress}>
-        Hello! This is my first react native app!
-      </Text>
-      {/* <Image source={require("./assets/icon.png")} /> */}
-      <TouchableOpacity onPress={() => console.log("Image Tapped")}>
-        <Image
-          fadeDuration={1000}
-          source={{
-            width: 200,
-            height: 300,
-            uri: "https://picsum.photos/200/300",
-          }}
-        />
-      </TouchableOpacity>
-      <View
-        style={{
-          width: "100%",
-          height: landscape ? "100%" : "30%",
-          backgroundColor: "dodgerblue",
-        }}
-      ></View>
-      <Button
-        color="orange"
-        title="Click Me"
-        onPress={() =>
-          Alert.alert("My title", "My message", [
-            { text: "Yes", onPress: () => console.log("Yes") },
-            { text: "No", onPress: () => console.log("No") },
-          ])
-        }
-      />
-    </SafeAreaView>
-  );
+  return <ViewImageScreen />;
 }
-
-const containerStyle = { backgroundColor: "orange" };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: Platform.OS === "anroid" ? StatusBar.currentHeight : 0,
-    // backgroundColor: "dodgerblue",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
